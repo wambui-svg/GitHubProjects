6361b0d380ea3522c3266861d6e12b5d5435f66a
wambui-svg <ruthwambui729@gmail.com>
2024-03-12 12:32:01 +0300

    new js file

---
+++ new.js
@@ -0,0 +1,7 @@
+   const contentArea = document.getElementById('content');
+    const changeColorBtn = document.getElementById('changeColorBtn');
+
+    changeColorBtn.addEventListener('click', function() {
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Generate a random hex color
+        contentArea.style.backgroundColor = randomColor;
+    });
